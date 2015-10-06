e065f6b477efff9ede47e4e242cddf149feea148
Steven Edouard <sedouard@microsoft.com>
2015-10-06 14:39:07 -0700

    wip

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    //favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+require('./modules/passport-init');
+
+// routes
+var routes = require('./routes/index'),
+    auth = require('./routes/auth/auth'),
+    users = require('./routes/users');
 
 var app = express();
 
@@ -18,19 +22,22 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -38,23 +45,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,32 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    passport = require('passport');
 
 /* GET home page. */
 router.get('/', function (req, res) {
     res.render('index', {
         title: 'Express'
     });
+});
+
+router.get('/success', function (req, res) {
+    res.render('index', {
+        title: 'Logged in!'
+    });
+});
+
+router.get('/login', passport.authenticate('azuread-openidconnect', {
+        failureRedirect: '/login'
+    }),
+    function (req, res) {
+        console.log('login was called!');
+        res.redirect('/success');
+    }
+);
+
+router.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
