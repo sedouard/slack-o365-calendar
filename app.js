c4016f3c8c7c89394564c852f0f3282277020a30
Steven Edouard <sedouard@microsoft.com>
2015-10-07 11:49:11 -0700

    fix(devops/jscs) - codestyle validations

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     path = require('path'),
-    //favicon = require('serve-favicon'),
+    // favicon = require('serve-favicon'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
@@ -21,7 +21,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -75,6 +76,5 @@ app.use(function (err, req, res) {
         error: {}
     });
 });
-
 
 module.exports = app;
